436223ebdc7075a98c6bfcb964d99423090caab2
Ander-Labs <119770203+Ander-Labs@users.noreply.github.com>
2023-01-09 12:14:04 -0500

    Add files via upload

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,2 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".)
+console.log("Hola Mundo");
+console.log("holaaa");
---
+++ index.js
@@ -0,0 +1,9 @@
+// este es el mensaje de vienvenidad
+console.log("Esta es la puerta del proyecto");
+
+/*este es la parte intermedia del codigo*/
+
+
+
+//este el es mensaje de despedida
+console.log("Bay");
